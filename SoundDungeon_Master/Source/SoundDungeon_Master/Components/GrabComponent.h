b430c47a94bbcc318bc92b26564fa6458d183de2
puschie286 <christoph286@googlemail.com>
2015-05-01 02:15:13 +0200

    Init - BaseProjekt für Code-Pipeline Added GrabComponent Extend FirstPerson-Blueprint with GrabComponent use example

---
+++ SoundDungeon_Master/Source/SoundDungeon_Master/Components/GrabComponent.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "GrabComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SOUNDDUNGEON_MASTER_API UGrabComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabComponent();
+
+	// Called when the game starts
+	virtual void InitializeComponent() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+	virtual void OnComponentDestroyed() override;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = Gameplay )
+	bool UsePhysicGrab;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = Gameplay )
+	float GrabRange;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = Gameplay )
+	FVector GrabDistance;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = Gameplay )
+	FName GrabTag;
+
+	UFUNCTION( BlueprintCallable, Category = "Grab" )
+	void GrabObject();
+
+	UFUNCTION( BlueprintCallable, Category = "Grab" )
+	void ReleaseObject();
+
+	UFUNCTION( BlueprintPure, Category = "Grab" )
+	bool HasGrabedObject();
+
+protected:
+
+	class UPhysicsHandleComponent* PhysicHandle;
+	class AActor* GrabedTarget;
+};
---
+++ SoundDungeon_Master/Source/SoundDungeon_Master/SoundDungeon_Master.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SoundDungeon_Master.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SoundDungeon_Master, "SoundDungeon_Master" );
